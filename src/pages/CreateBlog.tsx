ee642f87ee5ad8b122f1c483fa6f1feb51e5cd19
Jayson Baribar <jaysontbaribar@gmail.com>
2026-01-08 22:43:05 +0800

    Finished create / edit page structure

--- src/pages/CreateBlog.tsx
+++ src/pages/CreateBlog.tsx
@@ -1,9 +1,63 @@
+import { useState } from "react";
 import Page from "../components/layout/Page";
 
 
 const CreateBlog = () => {
 
-    return <h1> CreateBlogs </h1>;
+    const [title, setTitle]= useState("");
+    const [content, setContent] = useState("");
+
+    const handleSubmit = (e:React.FormEvent)=>{
+
+        e.preventDefault();
+
+        if (!title || !content){
+            alert(" Title and content are required. ")
+            return;
+        }   
+
+        console.log("New blog created: ", {
+            title,
+            content
+        });
+
+        alert ("Blog successfully created!")
+        setTitle("");
+        setContent("");
+    }
+
+
+    return (
+
+        <Page>
+        <h1> Create a new story! </h1>
+        <p> Write something you want to share with others. </p>
+
+        <form onSubmit = {handleSubmit} >
+            <div>
+                <label> Title </label>
+                <input 
+                type = "text"
+                value = {title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Enter your story title"
+          />
+            </div>
+
+            <div>
+                <label> Content </label>
+                    <textarea
+                    value = {content}
+                    placeholder="Write your story here "
+                    onChange={(e) => setContent(e.target.value)}
+                    />
+                
+            </div>
+
+            <button type = "submit" > Publish </button>
+        </form>
+        </Page>
+    )
 }
 
 export default CreateBlog;
--- src/pages/EditBlog.tsx
+++ src/pages/EditBlog.tsx
@@ -1,8 +1,73 @@
+import { useState, useEffect } from "react";
 import Page from "../components/layout/Page";
+
 
 const EditBlog = () => {
 
-    return <h1> EditBlogs </h1>;
+    const [title, setTitle]= useState("");
+    const [content, setContent] = useState("");
+
+    useEffect( ()=>{
+
+        const existingBlog ={
+            title: "My first blog",
+            content: "Content of the blog"
+        };
+
+        setTitle(existingBlog.title);
+        setContent(existingBlog.content);
+    }, []);
+
+    
+    const handleSubmit = (e:React.FormEvent)=>{
+
+        e.preventDefault();
+
+        if (!title || !content){
+            alert(" Title and content are required. ")
+            return;
+        }   
+
+        console.log("Blog updated!: ", {
+            title,
+            content
+        });
+
+        alert ("Blog successfully updated!")
+    }
+
+
+    return (
+
+        <Page>
+        <h1> Update Your Blog </h1>
+        <p>update your story and save the changes. </p>
+
+        <form onSubmit = {handleSubmit} >
+            <div>
+                <label> Title </label>
+                <input 
+                type = "text"
+                value = {title}
+                onChange={(e) => setTitle(e.target.value)}
+                placeholder="Enter your story title"
+          />
+            </div>
+
+            <div>
+                <label> Content </label>
+                    <textarea
+                    value = {content}
+                    placeholder="Write your story here "
+                    onChange={(e) => setContent(e.target.value)}
+                    />
+                
+            </div>
+
+            <button type = "submit" > Publish </button>
+        </form>
+        </Page>
+    )
 }
 
 export default EditBlog;
